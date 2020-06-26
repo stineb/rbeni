27a9562d2eba4de2dbe743a91fd49251279178c1
stineb <b.stocker@imperial.ac.uk>
2020-06-26 10:43:35 +0200

    added option for specifying ilon

--- R/nclist_to_df.R
+++ R/nclist_to_df.R
@@ -1,13 +1,16 @@
 #' Extracts full time series
 #'
 #' Extracts full time series from a list of NetCDF files, provided for time
-#' steps (can be one time step or multiple time steps) separately.
+#' steps (can be one time step or multiple time steps) separately and writes 
+#' .RData files for each longitude index.
 #'
 #' @param nclist A vector of character strings specifying the complete paths to files.
 #' @param outdir A character string specifying output directory where data frames are written
 #' using the \code{save} statement.
 #' @param fileprefix A character string specifying the file name prefix.
 #' @param varnam The variable name(s) for which data is to be read from NetCDF files.
+#' @param ilon An integer specifying an individual longitude index. If provided, it overrides that 
+#' the function extracts data for all longitude indices.
 #' @param lonnam The dimension name of longitude.
 #' @param latnam The dimension name of latitude
 #' @param timenam The name of dimension variable used for time. Defaults to \code{"time"}.
@@ -18,17 +21,23 @@
 #' base date (e.g., time units given in 'days since <basedate>').
 #' @param fgetdate A function to derive the date used for the time dimension based on the file name.
 #'
-#' @return Nothing. Writes data to files.
+#' @return Nothing. Writes data to .RData files for each longitude index.
 #' @export
 #'
-nclist_to_df <- function(nclist, outdir, fileprefix, varnam, lonnam = "lon", latnam = "lat", timenam = "time", timedimnam = "time", ncores = 1, single_basedate = FALSE, fgetdate = NA){
+nclist_to_df <- function(nclist, outdir, fileprefix, varnam, ilon = NA, lonnam = "lon", latnam = "lat", timenam = "time", timedimnam = "time", ncores = 1, single_basedate = FALSE, fgetdate = NA){
 
   require(dplyr)
   require(magrittr)
 
-  ## open one file to get longitude information
-  nlon <- ncmeta::nc_dim(nclist[1], lonnam) %>%
-    dplyr::pull(length)
+
+  ## Determine longitude indices
+  if (is.na(ilon)){
+    ## open one file to get longitude information
+    nlon <- ncmeta::nc_dim(nclist[1], lonnam) %>%
+      dplyr::pull(length)
+
+    ilon <- seq(nlon)
+  }  
 
   if (single_basedate && is.na(fgetdate)){
     ## get base date (to interpret time units in 'days since X')
@@ -45,7 +54,7 @@ nclist_to_df <- function(nclist, outdir, fileprefix, varnam, lonnam = "lon", lat
 
   ## collect time series per longitude slice and create separate files per longitude slice.
   ## This step can be parallelized (dependecies: tidync, dplyr, tidyr, purrr, magrittr)
-  if (ncores > 1){
+  if (ncores > 1 && length(ilon) > 1){
 
     cl <- multidplyr::new_cluster(ncores) %>%
       multidplyr::cluster_library(c("dplyr", "purrr", "tidyr", "tidync", "dplyr", "magrittr")) %>%
@@ -59,23 +68,23 @@ nclist_to_df <- function(nclist, outdir, fileprefix, varnam, lonnam = "lon", lat
       multidplyr::cluster_assign(timenam = timenam) %>%
       multidplyr::cluster_assign(timedimnam = timedimnam) %>%
       multidplyr::cluster_assign(fgetdate = fgetdate) %>%
-      multidplyr::cluster_assign(nclist_to_df_byidx = nclist_to_df_byidx)
+      multidplyr::cluster_assign(nclist_to_df_byilon = nclist_to_df_byilon)
 
     ## distribute to cores, making sure all data from a specific site is sent to the same core
-    df_out <- tibble(ilon = seq(nlon)) %>%
+    df_out <- tibble(ilon = ilon) %>%
       multidplyr::partition(cl) %>%
       dplyr::mutate(out = purrr::map_int( ilon,
-                                      ~nclist_to_df_byidx(nclist, ., outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate)))
+                                      ~nclist_to_df_byilon(nclist, ., outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate)))
 
   } else {
-    purrr::map(as.list(seq(nlon)), ~nclist_to_df_byidx(nclist, ., outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate))
+    purrr::map(as.list(ilon), ~nclist_to_df_byilon(nclist, ., outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate))
   }
 
 }
 
-nclist_to_df_byidx <- function(nclist, idx, outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate){
+nclist_to_df_byilon <- function(nclist, ilon, outdir, fileprefix, varnam, lonnam, latnam, basedate, timenam, timedimnam, fgetdate){
 
-  nclist_to_df_byfil <- function(filnam, idx, basedate, varnam, lonnam, latnam, timenam, timedimnam, fgetdate){
+  nclist_to_df_byfil <- function(filnam, ilon, basedate, varnam, lonnam, latnam, timenam, timedimnam, fgetdate){
 
     if (is.na(basedate) && is.na(fgetdate)){
       ## get base date (to interpret time units in 'days since X')
@@ -89,7 +98,7 @@ nclist_to_df_byidx <- function(nclist, idx, outdir, fileprefix, varnam, lonnam, 
     }
 
     df <- tidync::tidync(filnam) %>%
-      tidync::hyper_filter(lon = index == idx) %>%
+      tidync::hyper_filter(lon = index == ilon) %>%
       tidync::hyper_tibble(select_var(varnam))
 
     if (nrow(df)>0){
@@ -113,16 +122,16 @@ nclist_to_df_byidx <- function(nclist, idx, outdir, fileprefix, varnam, lonnam, 
 
   ## check whether output has been created already (otherwise do nothing)
   if (!dir.exists(outdir)){system(paste0("mkdir -p ", outdir))}
-  outpath <- paste0(outdir, fileprefix, "_ilon_", idx, ".RData")
+  outpath <- paste0(outdir, fileprefix, "_ilon_", ilon, ".RData")
 
   if (!file.exists(outpath)){
 
     print(paste("Getting file", outpath, "..."))
 
-    ## get data from all files at given longitude index idx
+    ## get data from all files at given longitude index ilon
     df <- purrr::map(
       as.list(nclist),
-      ~nclist_to_df_byfil(., idx, basedate = basedate, varnam = varnam, lonnam = lonnam, latnam = latnam, timenam = timenam, timedimnam = timedimnam, fgetdate)
+      ~nclist_to_df_byfil(., ilon, basedate = basedate, varnam = varnam, lonnam = lonnam, latnam = latnam, timenam = timenam, timedimnam = timedimnam, fgetdate)
       )
 
     ## chech if any element has zero rows and drop that element
@@ -144,7 +153,7 @@ nclist_to_df_byidx <- function(nclist, idx, outdir, fileprefix, varnam, lonnam, 
     print(paste("File exists already:", outpath))
   }
 
-  return(idx)
+  return(ilon)
 }
 
 
