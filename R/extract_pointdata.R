d2ea32d20dbc5b7ac3ca5a5e68e9aa11da4b2ca4
stineb <benjamin.stocker@gmail.comm>
2020-07-01 17:55:51 +0200

    asdf

--- R/extract_pointdata.R
+++ R/extract_pointdata.R
@@ -6,35 +6,40 @@
 #' @param filename A numeric specifying the longitude of the point for which data is to be read.
 #' @param df_lonlat A data frame containing columns \code{"lon"} (longitude in degrees) and \code{"lat"}
 #' (latitude in degrees)
-#' @param get_time A logical specifying whether time values are to be read and combined with nested
-#' data frames.
+#' @param time A logical specifying whether file has a time dimension.
 #' @return A data frame with columns \code{"lon"} and \code{"lat"} and a separate column for data.
 #' @export
 #'
-extract_pointdata_allsites <- function( filename, df_lonlat, get_time = FALSE ){
+extract_pointdata_allsites <- function( filename, df_lonlat, time = FALSE ){
 
   ## load file using the raster library
   #print(paste("Creating raster brick from file", filename))
   if (!file.exists(filename)) rlang::abort(paste0("File not found: ", filename))
-  rasta <- raster::brick(filename)
+  if (time){
+    rasta <- raster::brick(filename) 
+  } else {
+    rasta <- raster::raster(filename)
+  }
 
-  df_lonlat <- raster::extract(
+  df <- raster::extract(
     rasta,
     sp::SpatialPoints(dplyr::select(df_lonlat, lon, lat)), # , proj4string = rasta@crs
     sp = TRUE
-  ) %>%
-    as_tibble() %>%
-    tidyr::nest(data = c(-lon, -lat)) %>%
-    right_join(df_lonlat, by = c("lon", "lat")) %>%
-    mutate( data = purrr::map(data, ~dplyr::slice(., 1)) ) %>%
-    dplyr::mutate(data = purrr::map(data, ~t(.))) %>%
-    dplyr::mutate(data = purrr::map(data, ~as_tibble(.)))
+    ) %>% 
+    as_tibble()
+  
+  if (time){
+    df <- df %>%
+      tidyr::nest(data = c(-lon, -lat)) %>%
+      right_join(df_lonlat, by = c("lon", "lat")) %>%
+      mutate( data = purrr::map(data, ~dplyr::slice(., 1)) ) %>%
+      dplyr::mutate(data = purrr::map(data, ~t(.))) %>%
+      dplyr::mutate(data = purrr::map(data, ~as_tibble(.)))
 
-  if (get_time){
     timevals <- raster::getZ(rasta)
-    df_lonlat <- df_lonlat %>%
+    df <- df %>%
       mutate( data = purrr::map(data, ~bind_cols(., tibble(date = timevals))))
   }
 
-  return(df_lonlat)
+  return(df)
 }
