8f36bc41b222085a8cd780de0d2304bcac2968d4
stineb <b.stocker@imperial.ac.uk>
2022-07-30 11:14:29 +0200

    added spatial aggregation function for data frames

---
+++ R/bin_to_grid.R
@@ -0,0 +1,47 @@
+#' Bin data to rectangular grid
+#'
+#' Bin data into a rectangular geographical grid by longitude and latitude.
+#'
+#' @param df A data frame containing columns
+#' \code{lon} and \code{lat}
+#' @param from_lon left edge of grid (degrees longitude E)
+#' @param from_lat bottom edge of grid (degrees latitude N)
+#' @param to_lon right edge of grid (degrees longitude E)
+#' @param to_lat top edge of grid (degrees latitude N)
+#' @param dlon grid spacing in longitude (degrees)
+#' @param dlat grid spacing in latitude (degrees)
+#' @return A data frame where all numeric columns are aggregated by mean (na.rm = TRUE)
+#'
+#' @export
+#'
+#' @examples
+#'
+bin_to_grid <- function(df, from_lon, from_lat, to_lon, to_lat, dlon, dlat){
+
+  lon_breaks <- seq(from = from_lon, to = to_lon, by = dlon)
+  lat_breaks <- seq(from = from_lat, to = to_lat, by = dlat)
+
+  df <- df %>%
+    ungroup() %>%
+    mutate(ilon = cut(lon,
+                      breaks = lon_breaks),
+           ilat = cut(lat,
+                      breaks = lat_breaks)) %>%
+    mutate(lon_lower = as.numeric( sub("\\((.+),.*", "\\1", ilon)),
+           lon_upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", ilon) ),
+           lat_lower = as.numeric( sub("\\((.+),.*", "\\1", ilat) ),
+           lat_upper = as.numeric( sub("[^,]*,([^]]*)\\]", "\\1", ilat) )) %>%
+    mutate(lon_mid = (lon_lower + lon_upper)/2,
+           lat_mid = (lat_lower + lat_upper)/2) %>%
+
+    ## create cell name to associate with climate input
+    dplyr::select(-ilon, -ilat, -lon_lower, -lon_upper, -lat_lower, -lat_upper, -lon, -lat)
+
+  df_agg <- df %>%
+    group_by(lon_mid, lat_mid) %>%
+    summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+    rename(lon = lon_mid, lat = lat_mid)
+
+  return(df_agg)
+
+}
