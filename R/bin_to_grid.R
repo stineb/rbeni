85d63c67fdb3ede7a2a8070c33d4ad1ceccc2008
stineb <b.stocker@imperial.ac.uk>
2022-09-10 17:54:15 +0200

    updated logo

--- R/bin_to_grid.R
+++ R/bin_to_grid.R
@@ -10,13 +10,15 @@
 #' @param to_lat top edge of grid (degrees latitude N)
 #' @param dlon grid spacing in longitude (degrees)
 #' @param dlat grid spacing in latitude (degrees)
+#' @param do_nest A logical specifying whether data frame should be nested
+#' @param do_summarise A logical specifying whether data frame should be aggregated by a function
 #' @return A data frame where all numeric columns are aggregated by mean (na.rm = TRUE)
 #'
 #' @export
 #'
 #' @examples
 #'
-bin_to_grid <- function(df, from_lon, from_lat, to_lon, to_lat, dlon, dlat){
+bin_to_grid <- function(df, from_lon, from_lat, to_lon, to_lat, dlon, dlat, do_nest = FALSE, do_summarise = TRUE){
 
   lon_breaks <- seq(from = from_lon, to = to_lon, by = dlon)
   lat_breaks <- seq(from = from_lat, to = to_lat, by = dlat)
@@ -37,11 +39,22 @@ bin_to_grid <- function(df, from_lon, from_lat, to_lon, to_lat, dlon, dlat){
     ## create cell name to associate with climate input
     dplyr::select(-ilon, -ilat, -lon_lower, -lon_upper, -lat_lower, -lat_upper, -lon, -lat)
 
-  df_agg <- df %>%
-    group_by(lon_mid, lat_mid) %>%
-    summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
-    rename(lon = lon_mid, lat = lat_mid)
-
+  if (do_nest){
+    
+    df_agg <- df %>%
+      rename(lon = lon_mid, lat = lat_mid) %>% 
+      group_by(lon, lat) %>%
+      nest()
+    
+  } else if (do_summarise){
+    
+    df_agg <- df %>%
+      group_by(lon_mid, lat_mid) %>%
+      summarise(across(where(is.numeric), ~mean(.x, na.rm = TRUE))) %>%
+      rename(lon = lon_mid, lat = lat_mid)
+    
+  }
+  
   return(df_agg)
 
 }
